5d9350a0092d656b19a3c9503b486bd87d824adc
sefaapinar <sefanur.pinar@gmail.com>
2022-02-08 14:59:57 +0300

    User Routing Process

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { UsersComponent } from './users/users.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+const appRoutes: Routes=[
+
+  {path:'', component: HomeComponent}, //localhost:4200
+  {path:'home', component: HomeComponent},
+  {path:'products', component: ProductsComponent},
+  {path:'users', component: UsersComponent},
+  {path:'**', component: NotfoundComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    UsersComponent,
+    CategoriesComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
